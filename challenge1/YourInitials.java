2dce7a2c490dd05e5345884b2e0dcd9f1489a6a3
doinyne <dean.lewis@clearroute.co.uk>
2022-11-01 16:52:47 +0000

    my initials

---
+++ challenge1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("DDDD   L    ");
+        System.out.println("D   D  L    ");
+        System.out.println("D   D  L    ");
+        System.out.println("D   D  L    ");
+        System.out.println("D   D  L    ");
+        System.out.println("D   D  L    ");
+        System.out.println("DDDD   LLLLL ");
+    }
+}
