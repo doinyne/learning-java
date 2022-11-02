b64318ad72dc1202404b882814d3391425b511fb
doinyne <dean.lewis@clearroute.co.uk>
2022-11-02 10:29:33 +0000

    bus

--- section-two/bus.java
+++ section-two/bus.java
@@ -1,11 +1,13 @@
 public class bus {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
         int passengers = 0;
         passengers += 9;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers -= 5;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers -= 4;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
+        System.out.println("end of shift");
     }
 }
