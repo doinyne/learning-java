83a9d23ec670ac842cbc06714086955767aa08b7
doinyne <dean.lewis@clearroute.co.uk>
2022-11-02 16:00:11 +0000

    modular

--- section-two/MathOperations.java
+++ section-two/MathOperations.java
@@ -8,6 +8,16 @@ public class MathOperations {
 
         double testScore = 6.7;
         double bonusMarks = 2.5;
+        
+        int counter = 0;
+        counter ++;
+        counter ++;
+        counter ++;
+        System.out.println("I count: " + counter);
+        counter --;
+        counter --;
+        counter --;
+        System.out.println("I count: " + counter);
 
         System.out.println("Fred and George collected " + (bagOfSweets1 + bagOfSweets2) + " sweets");
         System.out.println("There are " + (starsInTheMilkyWay + starsInAndromeda) + " stars in the Milky Way and Andromeda galaxies.");
@@ -23,20 +33,11 @@ public class MathOperations {
         System.out.println("Originally, Fred had  " + (bagOfSweets2 / bagOfSweets1) + " times more sweets than George");
         System.out.println("Andromeda has " + (starsInAndromeda / starsInTheMilkyWay) + " times more stars");
         System.out.println("Snape caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks));
-    }
+
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + ( 10 % 2));
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + ( 5 % 2 ));
+    }   
 }
 
-// Fred and George collected <result> sweets
-// There are <result> stars in the Milky Way and Andromeda galaxies.
-// With bonus marks, Ron scored <result>/10 on his potions test. Snape wasn't too thrilled
-
-// Fred has  <result>  more sweets than George
-// The Andromeda Galaxy has <result> more stars than the Milky Way
-// Snape ended up removing marks. Ron actually scored <result> on his potions test
-
-// Fred and George used a special spell to multiply their sweets to: <result>
-// Snape made an error on his excel, accidentally multiplying Ron's score to: <result>/10
-
-// Originally, Fred had  <result> times more sweets than George
-// Andromeda has <result> times more stars
-// Snape caught the error. Furious, he divided Ron's score to: <result>
+// 10 is an even number, since dividing 10 by 2 has a remainder of: <remainder>
+// 5 is an odd number, since dividing 5 by 2 has a remainder of: <remainder>
