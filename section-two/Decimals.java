815a786373669990f8f1e9f0d802247fac9c70cb
doinyne <dean.lewis@clearroute.co.uk>
2022-11-02 14:41:04 +0000

    double is always 0.0

--- section-two/Decimals.java
+++ section-two/Decimals.java
@@ -2,5 +2,9 @@ public class Decimals {
     public static void main(String[] args) {
         double percentage = 35.5;
         System.out.println("In 2020, " + percentage + "% of the world liked potatoes!");
+
+        double divident = 25;
+        double divisor = 2;
+        System.out.println(divident/divisor);
     }
 }
