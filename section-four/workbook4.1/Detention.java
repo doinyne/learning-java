eb67dc61ed7ce602a90aee68392e7fa2e54633f0
doinyne <dean.lewis@clearroute.co.uk>
2022-11-04 16:55:16 +0000

    using a function to print multiple lines

---
+++ section-four/workbook4.1/Detention.java
@@ -0,0 +1,17 @@
+public class Detention {
+    public static void main(String[] args) {
+        printLine();
+        printLine();
+        printLine();
+        printLine();
+        printLine();
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names\n");
+    }
+}
