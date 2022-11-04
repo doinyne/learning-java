a928afe198928c27358bfebccdc4e823007c8abb
doinyne <dean.lewis@clearroute.co.uk>
2022-11-04 16:26:47 +0000

    staic function

---
+++ section-four/Chores.java
@@ -0,0 +1,12 @@
+public class Chores {
+    public static void main(String[] args) {
+        singChores();
+    }
+
+    public static void singChores() {
+       System.out.println("first line of chores");
+       System.out.println("second line of chores");
+       System.out.println("third line of chores");
+       System.out.println("forth line of chores\n");
+    }
+}
