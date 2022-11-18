d5f135fbb7240a96e0689f1c833770c75fd5987b
doinyne <dean.lewis@clearroute.co.uk>
2022-11-18 17:10:18 +0000

    section3 files

---
+++ section-four/JavaDraw.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class JavaDraw {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nWhich animal ");
+
+        scan.close();
+    }
+}
