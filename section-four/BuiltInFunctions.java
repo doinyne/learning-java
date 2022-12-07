8a23d9a2684610143a13b40fa42012a44e98d1b1
doinyne <dean.lewis@clearroute.co.uk>
2022-12-07 16:23:47 +0000

    workbook4.5

---
+++ section-four/BuiltInFunctions.java
@@ -0,0 +1,12 @@
+public class BuiltInFunctions {
+  public static void main(String[] args) {
+    System.out.println("hello java");
+
+    double sine = Math.sin(1.2);
+    //sin() returns the trigonometry sine of an angle in between 0.0 and pi.
+     System.out.println("the sin of 1.2 is: " + sine);
+
+     double power = Math.pow(2, 4);
+     System.out.println("2 to the power of 4 is: " + power);
+  }
+}
---
+++ section-four/workbook4.5/AreaCalculator.java
@@ -0,0 +1,55 @@
+public class AreaCalculator {
+
+  static double side = 2;
+  static double lenght = 1;
+  static double width = 2;
+  static double base = 1;
+  static double height = 2;
+  static double radius = 2;
+  public static void main(String[] args) {
+    printAreas();
+  }
+
+  public static void areaSquare(double side) {
+    if(side < 0) {
+      System.out.println("Impossible");
+    } else {
+      double areaOfSquare = side * 2;
+      System.out.println("the area of your square is: " + areaOfSquare);
+    }
+  }
+
+  public static void areaRectangle(double length, double width) {
+    if(length < 0 || width < 0) {
+      System.out.println("Error: Impossible");
+    } else {
+      double areaOfRectangle = length * width;
+      System.out.println("the area of your rectangle is: " + areaOfRectangle);
+    }
+  }
+
+  public static void areaTriangle(double base, double height) {
+    if(base < 0 || height < 0) {
+      System.out.println("Error: Impossible");
+    }else {
+      double areaOfTriangle = base * height / 2;
+      System.out.println("the area of your triangle is: " + areaOfTriangle);
+    }
+  }
+  
+  public static void areaCircle(double radius) {
+    if(radius < 0) {
+      System.out.println("Error: Impossible");
+    } else {
+      double areaOfCircle = Math.PI * radius * 2;
+      System.out.println("the area of your circle is: " + areaOfCircle);
+    }
+  }
+
+  public static void printAreas() {
+    areaSquare(side);
+    areaRectangle(lenght, width);
+    areaTriangle(base, height);
+    areaCircle(radius);
+  }
+}
