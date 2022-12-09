b2996c007c9d5d5b80b881bd81fe34a3a82c4144
doinyne <dean.lewis@clearroute.co.uk>
2022-12-09 09:56:14 +0000

    wip

--- section-four/challenge4/RockPaperScissors.java
+++ section-four/challenge4/RockPaperScissors.java
@@ -4,15 +4,19 @@ public class RockPaperScissors {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Let's play Rock Paper Scissors.\nWhen I say 'shoot', choose: rock, paper or scissors!\nAre you ready? Write 'yes' if you are");
+    System.out.println("Let's play Rock Paper Scissors.");
+    System.out.println("Are you ready? Write 'yes' if you are");
     
     String answer = scan.nextLine();
     
     if(answer.equals("yes")) {
-      System.out.println("Great!\nrock - paper - scissors, shoot!");
+      System.out.println("Great!\nWhen I say 'shoot', choose: rock, paper or scissors!");
+      System.out.println("rock - paper - scissors, shoot!");
     } else {
       System.out.println("Darn, some other time...!");
     }
+
+    
 
     String userWeapon = scan.nextLine();
 
@@ -41,17 +45,17 @@ public class RockPaperScissors {
   }
 
   public static void results(String userWeapon, String computerChoice) {
-    if(userWeapon == "rock" && computerChoice == "scissors") {
+    if(userWeapon.equals("rock") && computerChoice.equals("scissors")) {
       System.out.println("You win!");
-    } else if (userWeapon == "paper" && computerChoice == "rock") {
+    } else if (userWeapon.equals("paper") && computerChoice.equals("rock")) {
       System.out.println("You win!");
-    } else if (userWeapon == "scissors" && computerChoice == "paper") {
+    } else if (userWeapon.equals("scissors") && computerChoice.equals("paper")) {
       System.out.println("You win!");
-    } else if (computerChoice == "rock" && userWeapon == "scissors") {
+    } else if (computerChoice.equals("rock") && userWeapon.equals("scissors")) {
       System.out.println("Computer wins!");
-    } else if (computerChoice == "paper" && userWeapon == "rock") {
+    } else if (computerChoice.equals("paper") && userWeapon.equals("rock")) {
       System.out.println("Computer wins!");
-    } else if (computerChoice == "scissors" && userWeapon == "paper") {
+    } else if (computerChoice.equals("scissors") && userWeapon.equals("paper")) {
       System.out.println("Computer wins!");
     } else if (userWeapon == computerChoice){
       System.out.println("It's a draw!");
