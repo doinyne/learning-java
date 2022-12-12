c049d5e2db66a7ac76e54eaf4bdce32db45c4665
doinyne <dean.lewis@clearroute.co.uk>
2022-12-12 13:46:27 +0000

    workbook5.1

---
+++ section-five/workbook5.1/Megaphone.java
@@ -0,0 +1,10 @@
+public class Megaphone {
+
+    public static void main(String[]args) {
+        int counter = 0;
+        for(int i = 0; i < 10; i++) {
+            counter += 1;
+            System.out.println(counter + ". If Java was easy, they would call it Python!");
+        }
+    }
+}
