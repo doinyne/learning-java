6cc024a90e86c899085e9581f3352ac25fb87917
doinyne <dean.lewis@clearroute.co.uk>
2022-12-14 18:01:09 +0000

    generate 1 random card

--- section-five/Pokerito.java
+++ section-five/Pokerito.java
@@ -2,6 +2,122 @@ import java.util.Scanner;
 
 public class Pokerito {
     public static void main(String[] args) {
+        System.out.println(randomNumber());
+    }
 
+    public static String randomNumber() {
+        double randomDouble = Math.random() * 13 + 1;
+        int randomInt = (int)randomDouble;
+
+        String randomCard;
+
+        switch(randomInt) {
+            case 1: randomCard = "   _____\n" +
+                        "  |A _  |\n" +
+                        "  | ( ) |\n" +
+                        "  |(_'_)|\n" +
+                        "  |  |  |\n" +
+                        "  |____V|\n";
+                break;
+            case 2:
+                randomCard = "   _____\n" +
+                        "  |2    |\n" +
+                        "  |  o  |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____Z|\n";
+                break;
+            case 3:
+                randomCard = "   _____\n" +
+                        "  |3    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____E|\n";
+                break;
+            case 4:
+                randomCard = "   _____\n" +
+                        "  |4    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  | o o |\n" +
+                        "  |____h|\n";
+                break;
+            case 5:
+                randomCard = "   _____ \n" +
+                        "  |5    |\n" +
+                        "  | o o |\n" +
+                        "  |  o  |\n" +
+                        "  | o o |\n" +
+                        "  |____S|\n";
+                break;
+            case 6:
+                randomCard = "   _____ \n" +
+                        "  |6    |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  |____6|\n";
+                break;
+            case 7:
+                randomCard = "   _____ \n" +
+                        "  |7    |\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |____7|\n";
+                break;
+            case 8:
+                randomCard = "   _____ \n" +
+                        "  |8    |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  |____8|\n";
+                break;
+            case 9:
+                randomCard = "   _____ \n" +
+                        "  |9    |\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |____9|\n";
+                break;
+            case 10:
+                randomCard = "   _____ \n" +
+                        "  |10  o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |___10|\n";
+                break;
+            case 11:
+                randomCard = "   _____ \n" +
+                        "  |J  ww|\n" +
+                        "  | o {)|\n" +
+                        "  |o o% |\n" +
+                        "  | | % |\n" +
+                        "  |__%%[|\n";
+                break;
+            case 12:
+                randomCard = "   _____\n" +
+                        "  |Q  ww|\n" +
+                        "  | o {(|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%O|\n";
+                break;
+            case 13:
+                randomCard = "   _____\n" +
+                        "  |K  WW|\n" +
+                        "  | o {)|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%>|\n";
+                break;
+            default: randomCard = "Sorry error";
+            break;
+        }
+        return randomCard;
     }
 }
