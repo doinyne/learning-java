683e1d85a1a9e4a4be771a9cd1451cef1c97a8be
doinyne <dean.lewis@clearroute.co.uk>
2023-01-13 18:12:39 +0000

    grocer store array

--- section-six/workbook6/Grocer.java
+++ section-six/workbook6/Grocer.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Grocer {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String[] aisles = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
-        System.out.println("\nDo you sell coffee?");
-        for(int i = 0; i < aisles.length; i++)
-            if (aisles[i].equals("coffee")) {
-                System.out.println("We have coffee in aisle: " + i);
+        System.out.println("Welcome to Java Grocers.");
+        System.out.println("\nWhat can I help you find?");
+        String userItem = scan.nextLine();
+
+        for (int i = 0; i < aisles.length; i++) {
+            if(userItem.equals(aisles[i])) {
+                System.out.println("We have that in aisle: " + i);
             }
+        }
     }
 }
