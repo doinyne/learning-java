871da57b681e2052fa9e4871c79eee0a8017c192
doinyne <dean.lewis@clearroute.co.uk>
2023-01-13 12:49:24 +0000

    arrays

---
+++ section-six/workbook6.1/IntroToArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"United", "Great", "Union"};
+        System.out.println(Arrays.toString(kingdoms));
+        System.out.println(kingdoms[0]);
+    }
+}
