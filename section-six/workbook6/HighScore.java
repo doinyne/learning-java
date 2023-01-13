10c65203b826d31b2b4f073635458e606b7e483b
doinyne <dean.lewis@clearroute.co.uk>
2023-01-13 19:07:04 +0000

    index number added

--- section-six/workbook6/HighScore.java
+++ section-six/workbook6/HighScore.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class HighScore {
     public static void main(String[] args) {
         int highScore = 0;
+        int seat = 0;
         int[] numbersArray = new int[10];
         for(int i = 0; i < numbersArray.length; i++){
             numbersArray[i] = randomNumber();
@@ -10,10 +11,15 @@ public class HighScore {
         for(int i = 0; i < numbersArray.length; i++) {
             if(numbersArray[i] > highScore) {
                 highScore = numbersArray[i];
+                seat = i;
             }
         }
-        System.out.println(Arrays.toString(numbersArray));
-        System.out.println(highScore);
+
+
+
+        System.out.println("Here are the scores: " + Arrays.toString(numbersArray));
+        System.out.println(highScore + " is the highest score");
+        System.out.println("Its the gentleman in seat: " + seat + ". Give that man a cookie");
     }
 
     public static int randomNumber() {
