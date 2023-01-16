fcb815f188751e85369070f4a94e06244ceb1e03
doinyne <dean.lewis@clearroute.co.uk>
2023-01-16 16:38:57 +0000

    array copy of

--- section-six/workbook6/ReferenceTrap.java
+++ section-six/workbook6/ReferenceTrap.java
@@ -11,5 +11,12 @@ public class ReferenceTrap {
         staffThisYear[2] = "someoneelse";
         System.out.println(Arrays.toString(staffLastYear));
         System.out.println(Arrays.toString(staffThisYear));
+
+        String[] staffLastYear2 = {"dean", "mattias", "sarndeep"};
+        String[] staffThisYear2 = Arrays.copyOf(staffLastYear2, staffLastYear2.length);
+        staffThisYear2[1] = "someonenew";
+
+        System.out.println(Arrays.toString(staffLastYear2));
+        System.out.println(Arrays.toString(staffThisYear2));
     }
 }
