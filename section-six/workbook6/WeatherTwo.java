0038d856e1bc2f380eb203b18e22559cbcdd6217
doinyne <dean.lewis@clearroute.co.uk>
2023-01-16 17:41:56 +0000

    weather wip

--- section-six/workbook6/WeatherTwo.java
+++ section-six/workbook6/WeatherTwo.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 public class WeatherTwo {
     public static void main(String[] args) {
         double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
-
         double[] fahrenheit = celciusToFahrenheit(celsius);
-        System.out.println(Arrays.toString(fahrenheit));
+        System.out.println(printTemperatures(fahrenheit, Arrays.toString(fahrenheit)));
     }
 
     public static double[] celciusToFahrenheit(double[] celsius) {
@@ -14,5 +13,13 @@ public class WeatherTwo {
             fahren[i] = celsius[i] * 1.8 + 32;
         }
         return fahren;
+    }
+    //work this out on your own, dont use the workbook!
+    public static double printTemperatures(double[] fahrenheit, String Fahrenheit) {
+        System.out.print(Fahrenheit + ": ");
+        for(int i = 0; 0 < fahrenheit.length; i++) {
+
+        }
+        return something;
     }
 }
