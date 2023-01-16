b421525976cfb0d8f262b915ef62697d63179f4b
doinyne <dean.lewis@clearroute.co.uk>
2023-01-16 16:21:57 +0000

    pizza delivery done

--- section-six/workbook6/PizzaDelivery.java
+++ section-six/workbook6/PizzaDelivery.java
@@ -5,24 +5,23 @@ public class PizzaDelivery {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("How many pizza toppings would you like?");
-
         int toppingsAmount = scan.nextInt();
         scan.nextLine();
-
         String[] toppings = new String[toppingsAmount];
         System.out.println("Great, enter each topping");
-
         for(int i = 0; i < toppingsAmount; i++){
             System.out.print(i + ". ");
             toppings[i] = scan.nextLine();
         }
-
-        for(int i = 0; i < toppings.length; i++) {
-        }
-
-
-
         System.out.println("Thank you, here are the toppings you ordered: ");
+        for(int i = 0; i < toppings.length; i++) {
+            System.out.println(i + ". " + toppings[i]);
+        }
+        System.out.println("\nPress anything to confirm your order.");
+        scan.nextLine();
+        System.out.println("Great, a driver is on the way!");
+
+        scan.close();
     }
 }
 
