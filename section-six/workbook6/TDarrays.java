da25f871e56cd3c152e167489a26e0e6ee617d5d
doinyne <dean.lewis@clearroute.co.uk>
2023-01-18 18:37:54 +0000

    2d arrays wip

---
+++ section-six/workbook6/TDarrays.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class TDarrays {
+    public static void main(String[] args) {
+        int[][] grades = new int[3][4];
+        System.out.println("\tDean: " + Arrays.toString(grades[0]));
+        System.out.println("\tRom: " + Arrays.toString(grades[1]));
+        System.out.println("\tSomeone: " + Arrays.toString(grades[2]));
+    }
+}
