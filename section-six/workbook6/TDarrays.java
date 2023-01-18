bb35324ba5bb4348ce780bb904cca4a7de7d4267
doinyne <dean.lewis@clearroute.co.uk>
2023-01-18 20:26:50 +0000

    2d arrays

--- section-six/workbook6/TDarrays.java
+++ section-six/workbook6/TDarrays.java
@@ -2,9 +2,14 @@ import java.util.Arrays;
 
 public class TDarrays {
     public static void main(String[] args) {
-        int[][] grades = new int[3][4];
-        System.out.println("\tDean: " + Arrays.toString(grades[0]));
-        System.out.println("\tRom: " + Arrays.toString(grades[1]));
-        System.out.println("\tSomeone: " + Arrays.toString(grades[2]));
+        int[][] grades = {
+                {72,74,78,76},
+                {65,64,61,67},
+                {95,98,99,100},
+        };
+
+        System.out.println("\tDean: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        System.out.println("\tRom: " + grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+        System.out.println("\tSomeone: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
     }
 }
