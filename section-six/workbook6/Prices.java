dadf7be5a4b7b7dda2ef142186edff9e18d0432f
doinyne <dean.lewis@clearroute.co.uk>
2023-01-20 07:46:04 +0000

    nested for loops, advanced switch statement

--- section-six/workbook6/Prices.java
+++ section-six/workbook6/Prices.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Prices {
     public static void main(String[] args) {
         double[][] prices = {
@@ -7,8 +5,18 @@ public class Prices {
                 {0.99,1.99,2.49,1.49,2.99},
                 {8.99,7.99,9.49,9.99,10.99},
         };
-        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3]);
-        System.out.println("Beverage: " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3]);
-        System.out.println("Cereals: " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3]);
+        for(int i = 0; i < prices.length; i++) {
+
+            switch (i) {
+                case 0 -> System.out.print("\tSomeone: ");
+                case 1 -> System.out.print("\tSomeone: ");
+                case 2 -> System.out.print("\tSomeone: ");
+            }
+
+            for(int j = 0; j < prices[i].length; j++) {
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
     }
 }
