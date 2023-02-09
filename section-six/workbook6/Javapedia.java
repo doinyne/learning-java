909f0ade7f75aea1c750d3a2e62576d0c34826ca
doinyne <dean.lewis@clearroute.co.uk>
2023-02-09 17:25:57 +0000

    wip

--- section-six/workbook6/Javapedia.java
+++ section-six/workbook6/Javapedia.java
@@ -26,16 +26,21 @@ public class Javapedia {
         String[] newArray = new String[array.length];
         System.out.println(Arrays.deepToString(array));
         for(int i = 0; i <= array.length -1; i++) {
+
             newArray[i] = Arrays.toString(array[i]);
+
             System.out.println("Figure " + figureNumber++);
+
             System.out.print("Name > ");
             String Name = scan.nextLine();
-            newArray[i] =
+
             System.out.print("DOB > ");
             String dob = scan.nextLine();
+
             System.out.print("Occupancy: ");
             String occupancy = scan.nextLine();
         }
         System.out.println(Arrays.deepToString(array));
     }
 }
+
