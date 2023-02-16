1bca83cf54519b341358198e7a22c8d389f0186c
doinyne <dean.lewis@clearroute.co.uk>
2023-02-16 10:41:21 +0000

    wip

--- section-six/workbook6/Javapedia.java
+++ section-six/workbook6/Javapedia.java
@@ -3,6 +3,13 @@ import java.util.*;
 
 public class Javapedia {
     public static void main(String[] args) {
+
+
+
+    }
+
+    public static void enterInfo(String[][] array) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("How many historical figures will you register?");
         int userInput = scan.nextInt();
@@ -12,33 +19,31 @@ public class Javapedia {
 
         enterInfo(database);
 
-//        System.out.println("Name: " + database[0][0]);
-//        System.out.println("DOB: " + database[0][1]);
-//        System.out.println("Occupancy: " + database[0][2]);
-
-
         scan.close();
-    }
 
-    public static void enterInfo(String[][] array) {
-        Scanner scan = new Scanner(System.in);
         int figureNumber = 1;
+
         String[] newArray = new String[array.length];
         System.out.println(Arrays.deepToString(array));
+
         for(int i = 0; i <= array.length -1; i++) {
 
-            newArray[i] = Arrays.toString(array[i]);
+            for(int j = 0; j <= userInput; j++) {
 
-            System.out.println("Figure " + figureNumber++);
+                System.out.println("Figure " + figureNumber++);
 
-            System.out.print("Name > ");
-            String Name = scan.nextLine();
+                System.out.print("Name > ");
+                String Name = scan.nextLine();
+                newArray[j] = Name;
 
-            System.out.print("DOB > ");
-            String dob = scan.nextLine();
+                System.out.print("DOB > ");
+                String dob = scan.nextLine();
+                newArray[j] = dob;
 
-            System.out.print("Occupancy: ");
-            String occupancy = scan.nextLine();
+                System.out.print("Occupancy: ");
+                String occupancy = scan.nextLine();
+                newArray[j] = occupancy;
+            }
         }
         System.out.println(Arrays.deepToString(array));
     }
