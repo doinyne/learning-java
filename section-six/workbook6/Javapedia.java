f3d534b0a8c4ac12d7b5d7e4d938fb77b9eedebe
doinyne <dean.lewis@clearroute.co.uk>
2023-02-20 15:02:34 +0000

    fill arrays

--- section-six/workbook6/Javapedia.java
+++ section-six/workbook6/Javapedia.java
@@ -4,48 +4,32 @@ import java.util.*;
 public class Javapedia {
     public static void main(String[] args) {
 
+        enterInfo();
 
 
     }
 
-    public static void enterInfo(String[][] array) {
-
+    public static void enterInfo() {
         Scanner scan = new Scanner(System.in);
         System.out.println("How many historical figures will you register?");
         int userInput = scan.nextInt();
         scan.nextLine();
-
         String[][] database = new String[userInput][3];
+        System.out.println(Arrays.deepToString(database));
 
-        enterInfo(database);
-
-        scan.close();
-
-        int figureNumber = 1;
-
-        String[] newArray = new String[array.length];
-        System.out.println(Arrays.deepToString(array));
-
-        for(int i = 0; i <= array.length -1; i++) {
-
-            for(int j = 0; j <= userInput; j++) {
-
-                System.out.println("Figure " + figureNumber++);
-
-                System.out.print("Name > ");
-                String Name = scan.nextLine();
-                newArray[j] = Name;
-
-                System.out.print("DOB > ");
-                String dob = scan.nextLine();
-                newArray[j] = dob;
-
-                System.out.print("Occupancy: ");
-                String occupancy = scan.nextLine();
-                newArray[j] = occupancy;
+        for(int i = 0; i < database.length; i++) {
+            System.out.println("Figure " + (i + 1));
+            for(int j = 0; j < 1; j++) {
+                System.out.print("Name: ");
+                database[i][0] = scan.nextLine();
+                System.out.print("dob: ");
+                database[i][1] = scan.nextLine();
+                System.out.print("Occupation: ");
+                database[i][2] = scan.nextLine();
             }
+            System.out.println(Arrays.deepToString(database));
         }
-        System.out.println(Arrays.deepToString(array));
     }
+
 }
 
